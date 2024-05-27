9d11991a933c65633bf95f764240f9f6aed5fa13
zxcblog <zxc_7310@163.com>
2024-05-27 22:01:36 +0800

    feat: docker-compose添加kafka集群

--- framework/framework.go
+++ framework/framework.go
@@ -2,10 +2,13 @@ package framework
 
 import (
 	"github.com/zxcblog/rat-race/framework/gateway"
+	"github.com/zxcblog/rat-race/framework/grpc"
 )
 
 // Engine 框架的核心，用来启动 grpc 和 grpc-gateway 服务
 type Engine struct {
+	*grpc.Grpc
+
 	*gateway.Gateway
 }
 
@@ -17,5 +20,6 @@ func New() *Engine {
 }
 
 func (e *Engine) Run() {
+	e.Grpc.Run()
 	e.Gateway.Run()
 }
---
+++ framework/grpc/grpc.go
@@ -0,0 +1,12 @@
+package grpc
+
+type Grpc struct {
+}
+
+func New() *Grpc {
+	return &Grpc{}
+}
+
+func (g *Grpc) Run() {
+
+}
