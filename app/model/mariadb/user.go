bfbb888e96dd19c9cd655ef9f4cf0f835eff6c24
zxcblog <zxc_7310@163.com>
2024-06-18 23:17:17 +0800

    feat: 用户登录注册 proto 文件添加

--- app/model/mariadb/user.go
+++ app/model/mariadb/user.go
@@ -8,6 +8,7 @@ type User struct {
 	Email           string `gorm:"column:email;not null;comment:邮箱" json:"email"`
 	Mobile          string `gorm:"column:mobile;not null;comment:手机号" json:"mobile"`
 	EmailVerifiedAt uint32 `gorm:"column:email_verified_at;not null;comment:邮箱验证时间" json:"email_verified_at"`
+	RememberToken   string `gorm:"column:remember_token;not null;comment:记住我token" json:"remember_token"`
 	Status          uint   `gorm:"type:tinyint(1);column:status;not null;default:1;comment:状态 1:正常  2:禁用" json:"status"`
 
 	ModelTime
--- app/pb/user/user.pb.validate.go
+++ app/pb/user/user.pb.validate.go
@@ -35,120 +35,6 @@ var (
 	_ = sort.Sort
 )
 
-// Validate checks the field values on UserInfo with the rules defined in the
-// proto definition for this message. If any rules are violated, the first
-// error encountered is returned, or nil if there are no violations.
-func (m *UserInfo) Validate() error {
-	return m.validate(false)
-}
-
-// ValidateAll checks the field values on UserInfo with the rules defined in
-// the proto definition for this message. If any rules are violated, the
-// result is a list of violation errors wrapped in UserInfoMultiError, or nil
-// if none found.
-func (m *UserInfo) ValidateAll() error {
-	return m.validate(true)
-}
-
-func (m *UserInfo) validate(all bool) error {
-	if m == nil {
-		return nil
-	}
-
-	var errors []error
-
-	if utf8.RuneCountInString(m.GetAccount()) < 1 {
-		err := UserInfoValidationError{
-			field:  "Account",
-			reason: "value length must be at least 1 runes",
-		}
-		if !all {
-			return err
-		}
-		errors = append(errors, err)
-	}
-
-	// no validation rules for Nickname
-
-	// no validation rules for Mobile
-
-	if len(errors) > 0 {
-		return UserInfoMultiError(errors)
-	}
-
-	return nil
-}
-
-// UserInfoMultiError is an error wrapping multiple validation errors returned
-// by UserInfo.ValidateAll() if the designated constraints aren't met.
-type UserInfoMultiError []error
-
-// Error returns a concatenation of all the error messages it wraps.
-func (m UserInfoMultiError) Error() string {
-	var msgs []string
-	for _, err := range m {
-		msgs = append(msgs, err.Error())
-	}
-	return strings.Join(msgs, "; ")
-}
-
-// AllErrors returns a list of validation violation errors.
-func (m UserInfoMultiError) AllErrors() []error { return m }
-
-// UserInfoValidationError is the validation error returned by
-// UserInfo.Validate if the designated constraints aren't met.
-type UserInfoValidationError struct {
-	field  string
-	reason string
-	cause  error
-	key    bool
-}
-
-// Field function returns field value.
-func (e UserInfoValidationError) Field() string { return e.field }
-
-// Reason function returns reason value.
-func (e UserInfoValidationError) Reason() string { return e.reason }
-
-// Cause function returns cause value.
-func (e UserInfoValidationError) Cause() error { return e.cause }
-
-// Key function returns key value.
-func (e UserInfoValidationError) Key() bool { return e.key }
-
-// ErrorName returns error name.
-func (e UserInfoValidationError) ErrorName() string { return "UserInfoValidationError" }
-
-// Error satisfies the builtin error interface
-func (e UserInfoValidationError) Error() string {
-	cause := ""
-	if e.cause != nil {
-		cause = fmt.Sprintf(" | caused by: %v", e.cause)
-	}
-
-	key := ""
-	if e.key {
-		key = "key for "
-	}
-
-	return fmt.Sprintf(
-		"invalid %sUserInfo.%s: %s%s",
-		key,
-		e.field,
-		e.reason,
-		cause)
-}
-
-var _ error = UserInfoValidationError{}
-
-var _ interface {
-	Field() string
-	Reason() string
-	Key() bool
-	Cause() error
-	ErrorName() string
-} = UserInfoValidationError{}
-
 // Validate checks the field values on RegisterReq with the rules defined in
 // the proto definition for this message. If any rules are violated, the first
 // error encountered is returned, or nil if there are no violations.
@@ -171,9 +57,9 @@ func (m *RegisterReq) validate(all bool) error {
 
 	var errors []error
 
-	if utf8.RuneCountInString(m.GetAccount()) < 1 {
+	if utf8.RuneCountInString(m.GetUsername()) < 1 {
 		err := RegisterReqValidationError{
-			field:  "Account",
+			field:  "Username",
 			reason: "value length must be at least 1 runes",
 		}
 		if !all {
@@ -182,7 +68,17 @@ func (m *RegisterReq) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
-	// no validation rules for Nickname
+	if err := m._validateEmail(m.GetEmail()); err != nil {
+		err = RegisterReqValidationError{
+			field:  "Email",
+			reason: "value must be a valid email address",
+			cause:  err,
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
 
 	if utf8.RuneCountInString(m.GetPassword()) < 1 {
 		err := RegisterReqValidationError{
@@ -197,10 +93,6 @@ func (m *RegisterReq) validate(all bool) error {
 
 	// no validation rules for ConfirmPassword
 
-	// no validation rules for Mobile
-
-	// no validation rules for MobileCache
-
 	// no validation rules for ImgCache
 
 	if len(errors) > 0 {
@@ -208,6 +100,56 @@ func (m *RegisterReq) validate(all bool) error {
 	}
 
 	return nil
+}
+
+func (m *RegisterReq) _validateHostname(host string) error {
+	s := strings.ToLower(strings.TrimSuffix(host, "."))
+
+	if len(host) > 253 {
+		return errors.New("hostname cannot exceed 253 characters")
+	}
+
+	for _, part := range strings.Split(s, ".") {
+		if l := len(part); l == 0 || l > 63 {
+			return errors.New("hostname part must be non-empty and cannot exceed 63 characters")
+		}
+
+		if part[0] == '-' {
+			return errors.New("hostname parts cannot begin with hyphens")
+		}
+
+		if part[len(part)-1] == '-' {
+			return errors.New("hostname parts cannot end with hyphens")
+		}
+
+		for _, r := range part {
+			if (r < 'a' || r > 'z') && (r < '0' || r > '9') && r != '-' {
+				return fmt.Errorf("hostname parts can only contain alphanumeric characters or hyphens, got %q", string(r))
+			}
+		}
+	}
+
+	return nil
+}
+
+func (m *RegisterReq) _validateEmail(addr string) error {
+	a, err := mail.ParseAddress(addr)
+	if err != nil {
+		return err
+	}
+	addr = a.Address
+
+	if len(addr) > 254 {
+		return errors.New("email addresses cannot exceed 254 characters")
+	}
+
+	parts := strings.SplitN(addr, "@", 2)
+
+	if len(parts[0]) > 64 {
+		return errors.New("email address local phrase cannot exceed 64 characters")
+	}
+
+	return m._validateHostname(parts[1])
 }
 
 // RegisterReqMultiError is an error wrapping multiple validation errors
@@ -419,35 +361,6 @@ func (m *UserAuthRes) validate(all bool) error {
 	}
 
 	var errors []error
-
-	if all {
-		switch v := interface{}(m.GetUserinfo()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, UserAuthResValidationError{
-					field:  "Userinfo",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		case interface{ Validate() error }:
-			if err := v.Validate(); err != nil {
-				errors = append(errors, UserAuthResValidationError{
-					field:  "Userinfo",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		}
-	} else if v, ok := interface{}(m.GetUserinfo()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return UserAuthResValidationError{
-				field:  "Userinfo",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
-	}
 
 	if all {
 		switch v := interface{}(m.GetTokenInfo()).(type) {
